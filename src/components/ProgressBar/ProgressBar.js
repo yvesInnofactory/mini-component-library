aa167b82829c5a2df83d497fc1638be269412d5f
Yves Wälchli <yves@innofactory.ch>
2023-08-03 11:51:34 +0200

    Solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let containerHeight = "12px";
+  let borderRadius = "4px";
+  let progressPadding = "0";
+  if (size === "large") {
+    containerHeight = "24px";
+    borderRadius = "8px";
+    progressPadding = "4px";
+  }
+  if (size === "small") {
+    containerHeight = "8px";
+    borderRadius = "4px";
+    progressPadding = "0";
+  }
+
+  const ProgressContainer = styled.div`
+    height: ${containerHeight};
+    border-radius: ${borderRadius};
+    width: 100%;
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    padding: ${progressPadding};
+    overflow: hidden;
+  `;
+
+  const ProgressBar = styled.div`
+    background-color: ${COLORS.primary};
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    height: 100%;
+    width: ${value}%;
+  `;
+
+  return (
+    <ProgressContainer>
+      <ProgressBar></ProgressBar>
+    </ProgressContainer>
+  );
 };
 
 export default ProgressBar;
